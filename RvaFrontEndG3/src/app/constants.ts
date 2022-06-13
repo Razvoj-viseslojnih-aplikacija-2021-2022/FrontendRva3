4b5f274fbf1e721ae16e870f5ef31d98414bf8cb
Vladimir Fabri <vfficial98@gmail.com>
2022-06-13 13:51:49 +0200

    Vezbe 12-Dodaci za deployment

--- RvaFrontEndG3/src/app/constants.ts
+++ RvaFrontEndG3/src/app/constants.ts
@@ -1,5 +1,15 @@
-export const ARTIKL_URI = "http://localhost:8083/artikl";
+
+//Stare konstante koje smo koristili na vežbama, služe samo za lokalno pokretanje aplikacije
+/* export const ARTIKL_URI = "http://localhost:8083/artikl";
 export const DOBAVLJAC_URI = "http://localhost:8083/dobavljac";
 export const PORUDZBINA_URI = "http://localhost:8083/porudzbina";
 export const STAVKA_PORUDZBINE_URI = "http://localhost:8083/stavkaPorudzbine";
-export const STAVKE_FOR_PORUDZBINA_URI = "http://localhost:8083/stavkaPorudzbine/forPorudzbina";
+export const STAVKE_FOR_PORUDZBINA_URI = "http://localhost:8083/stavkaPorudzbine/forPorudzbina"; */
+
+
+//Nove konstante koje gadjaju endPoint-e na deploy-ovanom backend-u
+export const ARTIKL_URI = "https://testingbackendvf.herokuapp.com/artikl";
+export const DOBAVLJAC_URI = "https://testingbackendvf.herokuapp.com/dobavljac";
+export const PORUDZBINA_URI = "https://testingbackendvf.herokuapp.com/porudzbina";
+export const STAVKA_PORUDZBINE_URI = "https://testingbackendvf.herokuapp.com/stavkaPorudzbine";
+export const STAVKE_FOR_PORUDZBINA_URI = "https://testingbackendvf.herokuapp.com/stavkaPorudzbine/forPorudzbina";
